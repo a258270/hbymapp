96cf67e519e1997b4d0a5d67ae9d1c848c76498d
wns <wns@qq.com>
2017-11-29 09:14:29 +0800

    首页进行中

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,7 +12,7 @@ Page({
     // 首页轮播图 开始
     imgUrls: [
       '../../img/banner.jpg',
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      // 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
