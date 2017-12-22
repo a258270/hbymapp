834fc4a00a196f3a826ec7e370f889aba4e7da72
zmj <a258270@qq.com>
2017-12-22 14:08:39 +0800

    剔除tabbar

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,8 @@
 //index.js
-//获取应用实例
-import {
-  init,                    // 初始化组件及页面
-  Tabbar,                  // Tabbar是组件的事件注册中心
-  setTabbarData            // 设置/更新 tabbar显示的数据
-} from "../template/tabbar";
-
 var order = ['red', 'yellow', 'blue', 'green', 'red']
 const app = getApp()
 var utils = require('../../utils/util.js')
-const UserPageData = {
+Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
@@ -138,38 +131,4 @@ const UserPageData = {
       });
     })
   },
-}
-
-// tabbar 对象的数据
-const tabbarData = [
-  {
-    iCount: 98,                         //未读数目 Integer
-    sIconUrl: "/img/contact.png",     //按钮图标 String
-    sTitle: "联系人",                     //按钮名称 String
-  },
-  {
-    iCount: 0,
-    sIconUrl: "/img/homepages.png",
-    sTitle: "主页",
-  },
-  {
-    iCount: 0,
-    sIconUrl: "/img/personals.png",
-    sTitle: "我的",
-  }
-];
-
-
-// 调用 组件的设置函数，传入数据对象
-setTabbarData(tabbarData);
-
-// 调用已经封装好的启动函数 类似于 原生的Page
-// 已经封装所有的接口
-init(UserPageData);
-
-// 注册tab被单击时触发的事件
-Tabbar.addListener(function (ev) {
-  console.log(ev);
-  // ev.key === 'note'
-  // ev的key对应被点击的tab的title
 });
