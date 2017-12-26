b2f32c56415772f0f5400d6a0b9fc5184fb5639b
zmj <a258270@qq.com>
2017-12-26 11:33:34 +0800

    修改轮播图src

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,10 +10,9 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     // 首页轮播图 开始
     imgUrls: [
-      '../../img/banner.jpg',
-      // 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      utils.setStaticUrl("/static/ymplant/img/sye/banner/banner4.jpg"),
+      utils.setStaticUrl("/static/ymplant/img/sye/banner/banner5.jpg"),
+      utils.setStaticUrl("/static/ymplant/img/sye/banner/6.jpg"),
     ],
     indicatorDots: false,
     autoplay: true,
